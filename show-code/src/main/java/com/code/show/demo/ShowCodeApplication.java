b474e54f3490e79c86d2c6e531fe9908beb1ea98
fate-7 <32026536+fate-7@users.noreply.github.com>
2021-09-28 16:58:31 +0800

    upload code
    https://shimowendang.com/docs/DwRxjVY6vJhykdrc/ 《如何把mybatis整合到spring中? 》，可复制链接后用石墨文档 App 或小程序打开

---
+++ show-code/src/main/java/com/code/show/demo/ShowCodeApplication.java
@@ -0,0 +1,13 @@
+package com.code.show.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowCodeApplication.class, args);
+	}
+
+}
